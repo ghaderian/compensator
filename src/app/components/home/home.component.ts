a626e3be922bdd3818953a8a07a6ea9c99aada16
Houman Ghaderian <ghaderian@gmail.com>
2022-04-18 22:18:02 +1000

    submit on modal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestDocument } from 'src/app/core/model/request.document';
+import { RequestService } from 'src/app/core/request.service';
+import { CoreUtil } from 'src/app/core/util/coreUtil';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  isModalOpen = false;
-  fadeIn = false;
-  fadeOut = false;
+  email = '';
+  isEmailValid = true;
 
   modalState: 'fade-in' | 'fade-out' | 'visible' | 'hidden' = 'hidden'
 
-  constructor() { }
+  private service: RequestService
+
+  constructor(service: RequestService) {
+    this.service = service;
+  }
 
   ngOnInit(): void {
 
@@ -32,6 +38,26 @@ export class HomeComponent implements OnInit {
     setTimeout(() => {
       this.modalState = 'hidden'
     },1000);
+  }
+
+  submit() {
+
+    this.isEmailValid = CoreUtil.validateEmail(this.email);
+
+    if (this.isEmailValid) {
+
+      const req: RequestDocument = {
+        email : this.email,
+        family: '',
+        name: '',
+        time: Date.now()
+      }
+      this.service.createRequst(req);
+
+      this.closeModal();
+
+    }
+
   }
 
 }
